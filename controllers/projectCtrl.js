a538419532f0299d0a8edabf960990b936dc806f
Hojoon Kim <hojoon0724@gmail.com>
2024-02-21 18:34:22 -0800

    server running

--- controllers/projectCtrl.js
+++ controllers/projectCtrl.js
@@ -1,6 +1,7 @@
 // -----------------------------------------------------
 // Dependencies
 // -----------------------------------------------------
+const express = require('express')
 const Project = require('../models/Project')
 const router = express.Router()
 
